fab453a92c741c535fb2a9776bcfded227216205
chrisyang-tw <christopheryang2012@gmail.com>
2018-06-17 20:44:16 +0800

    t2

--- app.py
+++ app.py
@@ -47,9 +47,11 @@ def verify_fb_token(token_sent):
 
 #chooses a random message to send to the user
 def get_message():
-    sample_responses = ['1', '2', '3']
+    # sample_responses = ['1', '2', '3']
+    sample_responses = message
     # return selected item to the user
-    return random.choice(sample_responses)
+    # return random.choice(sample_responses)
+    return sample_responses
 
 #uses PyMessenger to send response to user
 def send_message(recipient_id, response):
