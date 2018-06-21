5b0a7cf136d93f997a506e96630a8cbc788f9ea5
chrisyang-tw <christopheryang2012@gmail.com>
2018-06-22 01:38:29 +0800

    QQ

--- app.py
+++ app.py
@@ -2,27 +2,31 @@
 import os
 import json
 from flask import Flask, request
-from fbmq import Attachment, Template, QuickReply, NotificationType, Page
+from fbmq import Page
 
 app = Flask(__name__)
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
+page = Page(ACCESS_TOKEN)
 
 #We will receive messages that Facebook sends our bot at this endpoint 
 @app.route("/", methods=['GET'])
-def webhook():
+def hello():
     # Before allowing people to message your bot, Facebook has implemented a verify token that confirms all requests that your bot receives came from Facebook.""" 
     if request.args.get("hub.verify_token") == VERIFY_TOKEN:
         return request.args.get("hub.challenge")
     else:
         return 'Invalid verification token'
 
+@app.route('/', methods=['POST'])
+def webhook():
+    page.handle_webhook(request.get_data(as_text=True))
+    return "ok"
+
 ####################################
 
-page = Page(ACCESS_TOKEN)
-
 @page.handle_message
-def message_handler(fbmq.Event):
+def message_handler(event):
   """:type event: fbmq.Event"""
   sender_id = event.sender_id
   message = event.message_text
@@ -30,7 +34,7 @@ def message_handler(fbmq.Event):
   page.send(sender_id, "thank you! your message is '%s'" % message)
 
 @page.after_send
-def after_send(fbmq.Payload, response):
+def after_send(payload, response):
   """:type payload: fbmq.Payload"""
   print("complete")
 
