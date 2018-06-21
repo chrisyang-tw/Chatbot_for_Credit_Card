5b0f3709e0f5edba7e0c944a5fe8ed681f66bc90
chrisyang-tw <christopheryang2012@gmail.com>
2018-06-22 01:46:30 +0800

    ccc

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import os
 import json
 from flask import Flask, request
-from fbmq import Page
+from fbmq import Attachment, Template, QuickReply, Page
 
 app = Flask(__name__)
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
@@ -27,16 +27,24 @@ def webhook():
 
 @page.handle_message
 def message_handler(event):
-  """:type event: fbmq.Event"""
-  sender_id = event.sender_id
-  message = event.message_text
-  
-  page.send(sender_id, "thank you! your message is '%s'" % message)
+    """:type event: fbmq.Event"""
+    sender_id = event.sender_id
+    message = event.message_text
+    if message == 'A':
+        page.send(sender_id, "thank you! your message is '%s'" % message)
+    elif message == 'B':
+        page.send(sender_id, Attachment.Image(http://i.imgur.com/hKORBJK.jpg))
+    elif message == 'C':
+        quick_replies = [
+            QuickReply(title="Action", payload="PICK_ACTION"),
+            QuickReply(title="Comedy", payload="PICK_COMEDY")
+        ]
+        page.send(sender_id, "What's your favorite movie genre?", quick_replies=quick_replies, metadata="DEVELOPER_DEFINED_METADATA")
 
 @page.after_send
 def after_send(payload, response):
-  """:type payload: fbmq.Payload"""
-  print("complete")
+    """:type payload: fbmq.Payload"""
+    print("complete")
 
 if __name__ == '__main__':
     app.run()
