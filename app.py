d95a72b2b4894469db0059b68958c9a51f84e0ca
chrisyang-tw <christopheryang2012@gmail.com>
2018-06-23 19:31:34 +0800

    005

--- app.py
+++ app.py
@@ -39,39 +39,25 @@ def received_postback(event):
     payload = event.postback_payload
 
     if payload == 'START_PAYLOAD':
-        page.send(sender_id, '各位潮潮的菁英們，實習都已經賺了那麼多錢，是不是覺得沒地方花呢？想不想成為一個卡奴呢？',
-                  quick_replies=[{'title':'想', 'payload':'Y'},
-                                 {'title':'不想', 'payload':'N'}])
+        page.send(sender_id, '各位潮潮的菁英們，實習都已經賺了那麼多錢，是不是覺得沒地方花呢？想不想Bang成一個快樂的卡奴呢？',
+                  quick_replies=[{'title': '想！', 'payload': 'Y'},
+                                 {'title': '不想', 'payload': 'N'}])
     # if payload == 'ABC':
     #     page.send(sender_id, '早安我的朋友')
     # elif payload == 'DEF':
     #     page.send(sender_id, '晚安我的朋友')
     # elif payload == 'DEVELOPED_DEFINED_PAYLOAD':
     #     page.send(sender_id, '噫！好了！我中了！')
-    # elif payload == 'MENU1':
-    #     page.send(sender_id, '母湯喔')
-    # elif payload == 'MENU2':
-    #     page.send(sender_id, '湯喔')
-    # elif payload == 'START_PAYLOAD':
-    #     page.send(sender_id, '在想些什麼？')
 
 ####################################
-##### 開始菜單(未完成)
+##### 開始訊息與菜單
 page.greeting('想變卡奴找我們準沒錯！')
 
 page.show_starting_button('START_PAYLOAD')
-# @page.callback(['START_PAYLOAD'])
-# def start_callback(payload, event):
-#     print("Let's start!")
 
 page.show_persistent_menu([Template.ButtonWeb("Open Web URL", "https://www.oculus.com/en-us/rift/"),
-                           Template.ButtonPostBack('MENU1', 'MENU1'),
-                           Template.ButtonPostBack('MENU2', 'MENU2')])
-
-# @page.callback(['MENU_PAYLOAD/(.+)'])
-# def click_persistent_menu(payload, event):
-#     click_menu = payload.split('/')[1]
-#     print("you clicked %s menu" % click_menu)
+                           Template.ButtonPostBack('開始使用', 'START_PAYLOAD'),
+                           Template.ButtonPostBack('Undefined', 'MENU2')])
 
 ####################################
 ##### 
@@ -81,43 +67,74 @@ def message_handler(event):
     sender_id = event.sender_id
     message = event.message_text
 
-    ##### 按鈕
-    if message == 'A':     
-        buttons = [{'type': 'web_url', 'title': 'Open Web URL', 'value': 'https://www.oculus.com/en-us/rift/'},
-                   {'type': 'postback', 'title': 'trigger Postback', 'value': 'DEVELOPED_DEFINED_PAYLOAD'},
-                   {'type': 'phone_number', 'title': 'Call Phone Number', 'value': '+886970119732'}]
-        page.send(sender_id, Template.Buttons("hello", buttons))
+    ## 第一次判斷
 
-    ##### 圖片
-    elif message == 'B':
-        page.send(sender_id, Attachment.Image('http://i.imgur.com/hKORBJK.jpg'))
+    
+    if message == '想':
+        page.send(sender_id, '那讓我們開始吧！首先先問問你希望想擁有的信用卡特色？',
+                  quick_replies=[{'title': '高額現金回饋', 'payload': 'cash'},
+                                 {'title': '旅遊交通', 'payload': 'traffic'},
+                                 {'title': '休閒娛樂', 'payload': 'entertain'},
+                                 {'title': '購物', 'payload': 'shopping'},
+                                 {'title': '電子支付功能', 'payload': 'easycard'},
+                                 {'title': '宗教', 'payload': 'religion'}])
+    elif message == '不想':
+        page.send(sender_id, '相信我，卡奴也是可以過得很快樂的，給你最後一次機會，你想不想成為卡奴？',
+                  quick_replies=[{'title':'想！', 'payload':'Y'}])
 
-    ##### 快速回覆
-    elif message == 'C':
-        quick_replies = [{'title': 'Action', 'payload': 'PICK_ACTION'},
-                         {'title': 'Comedy', 'payload': 'PICK_COMEDY'}]
-        page.send(sender_id, "What's your favorite movie genre?", quick_replies=quick_replies, metadata="DEVELOPER_DEFINED_METADATA")
+    ## 第二次判斷
+    sub_features = {'高額現金回饋': [{'title': '國內現金回饋', 'payload': 'in'},
+                                    {'title': '國外現金回饋', 'payload': 'out'}],
+                    '旅遊交通': [{'title': '里程累積', 'payload': 'meter'},
+                                {'title': '旅遊優惠', 'payload': 'travel'},
+                                {'title': '國外刷卡優惠', 'payload': 'outside'},
+                                {'title': '高鐵', 'payload': 'hsr'},
+                                {'title': '加油停車', 'payload': 'oil'},
+                                {'title': 'eTag', 'payload': 'etag'}],
+                    '休閒娛樂': [{'title': '美食', 'payload': 'food'},
+                                {'title': '電影', 'payload': 'movie'}],
+                    '購物': [{'title': '通路聯名', 'payload': 'chain'},
+                            {'title': '網路購物', 'payload': 'shopee'}]
+                    }
+    
+    elif message in sub_features:
+        page.send(sender_id, '再選擇一個子項目吧', quick_replies=sub_features[message])
+    # if message == 'A':     
+    #     buttons = [{'type': 'web_url', 'title': 'Open Web URL', 'value': 'https://www.oculus.com/en-us/rift/'},
+    #                {'type': 'postback', 'title': 'trigger Postback', 'value': 'DEVELOPED_DEFINED_PAYLOAD'},
+    #                {'type': 'phone_number', 'title': 'Call Phone Number', 'value': '+886970119732'}]
+    #     page.send(sender_id, Template.Buttons("hello", buttons))
 
-    ##### Generic Template
-    elif message == 'D':
-        page.send(sender_id, Template.Generic([
-                Template.GenericElement("rift",
-                                subtitle="Next-generation virtual reality",
-                                item_url="https://www.oculus.com/en-us/rift/",
-                                # image_url=CONFIG['SERVER_URL'] + "/assets/rift.png",
-                                image_url='http://i.imgur.com/hKORBJK.jpg',
-                                buttons=[{'type': 'web_url', 'title': 'Open Web URL', 'value': 'https://www.oculus.com/en-us/rift/'},
-                                        {'type': 'postback', 'title': 'BANK A', 'value': 'ABC'},
-                                        {'type': 'phone_number', 'title': 'Call Phone Number', 'value': '+886970119732'}]),
-                Template.GenericElement("touch",
-                                subtitle="Your Hands, Now in VR",
-                                item_url="https://www.oculus.com/en-us/touch/",
-                                image_url='http://i.imgur.com/hKORBJK.jpg',
-                                buttons=[
-                                        {'type': 'web_url', 'title': 'Open Web URL', 'value': 'https://www.oculus.com/en-us/rift/'},
-                                        {'type': 'postback', 'title': 'BANK B', 'value': 'DEF'},
-                                        {'type': 'phone_number', 'title': 'Call Phone Number', 'value': '+886970119732'}])
-        ]))
+    # ##### 圖片
+    # elif message == 'B':
+    #     page.send(sender_id, Attachment.Image('http://i.imgur.com/hKORBJK.jpg'))
+
+    # ##### 快速回覆
+    # elif message == 'C':
+    #     quick_replies = [{'title': 'Action', 'payload': 'PICK_ACTION'},
+    #                      {'title': 'Comedy', 'payload': 'PICK_COMEDY'}]
+    #     page.send(sender_id, "What's your favorite movie genre?", quick_replies=quick_replies, metadata="DEVELOPER_DEFINED_METADATA")
+
+    # ##### Generic Template
+    # elif message == 'D':
+    #     page.send(sender_id, Template.Generic([
+    #             Template.GenericElement("rift",
+    #                             subtitle="Next-generation virtual reality",
+    #                             item_url="https://www.oculus.com/en-us/rift/",
+    #                             # image_url=CONFIG['SERVER_URL'] + "/assets/rift.png",
+    #                             image_url='http://i.imgur.com/hKORBJK.jpg',
+    #                             buttons=[{'type': 'web_url', 'title': 'Open Web URL', 'value': 'https://www.oculus.com/en-us/rift/'},
+    #                                     {'type': 'postback', 'title': 'BANK A', 'value': 'ABC'},
+    #                                     {'type': 'phone_number', 'title': 'Call Phone Number', 'value': '+886970119732'}]),
+    #             Template.GenericElement("touch",
+    #                             subtitle="Your Hands, Now in VR",
+    #                             item_url="https://www.oculus.com/en-us/touch/",
+    #                             image_url='http://i.imgur.com/hKORBJK.jpg',
+    #                             buttons=[
+    #                                     {'type': 'web_url', 'title': 'Open Web URL', 'value': 'https://www.oculus.com/en-us/rift/'},
+    #                                     {'type': 'postback', 'title': 'BANK B', 'value': 'DEF'},
+    #                                     {'type': 'phone_number', 'title': 'Call Phone Number', 'value': '+886970119732'}])
+    #     ]))
         
     ##### 鸚鵡
     else:
---
+++ test.py
@@ -0,0 +1,13 @@
+sub_features = {'高額現金回饋': [{'title': '國內現金回饋', 'payload': 'in'},
+                                {'title': '國外現金回饋', 'payload': 'out'}],
+                '旅遊交通': [{'title': '里程累積', 'payload': 'meter'},
+                            {'title': '旅遊優惠', 'payload': 'travel'},
+                            {'title': '國外刷卡優惠', 'payload': 'outside'},
+                            {'title': '高鐵', 'payload': 'hsr'},
+                            {'title': '加油停車', 'payload': 'oil'},
+                            {'title': 'eTag', 'payload': 'etag'}],
+                '休閒娛樂': [{'title': '美食', 'payload': 'food'},
+                            {'title': '電影', 'payload': 'movie'}],
+                '購物': [{'title': '通路聯名', 'payload': 'chain'},
+                        {'title': '網路購物', 'payload': 'shopee'}]
+                }
