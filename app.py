8cffe176038bdc05db01afd14831d2614b62a5dd
chrisyang-tw <christopheryang2012@gmail.com>
2018-06-27 16:32:08 +0800

    test01

--- app.py
+++ app.py
@@ -92,7 +92,6 @@ def received_postback(event):
                   quick_replies=[{'title': '想！', 'payload': 'Y'},
                                  {'title': '不想', 'payload': 'N'}])
     else:
-        # page.send(sender_id, payload)
         page.send(sender_id, cal2(payload))
 
 ####################################
